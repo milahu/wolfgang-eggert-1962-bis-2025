fb78119b8f65c9003cc09f68d24a496da5fe49e0
Milan Hauth <milahu@gmail.com>
2025-12-28 20:44:46 +0100

    add scripts

---
+++ scripts/git-remote-add.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+owner=milahu
+repo=wolfgang-eggert-1962-bis-2025
+
+function git_remote_add_onion() {
+  local remote="$1"
+  local url="$2"
+  git remote add "$remote" "$url" ||
+  git remote set-url "$remote" "$url"
+  git config --add remote."$remote".proxy socks5h://127.0.0.1:9050
+}
+
+git_remote_add_onion darktea.onion http://it7otdanqu7ktntxzm427cba6i53w6wlanlh23v5i3siqmos47pzhvyd.onion/$owner/$repo
+
+git_remote_add_onion righttoprivacy.onion http://gg6zxtreajiijztyy5g6bt5o6l3qu32nrg7eulyemlhxwwl6enk6ghad.onion/$owner/$repo
